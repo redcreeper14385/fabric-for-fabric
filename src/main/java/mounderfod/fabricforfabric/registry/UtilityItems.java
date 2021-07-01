eec79c8a840cb808ba6fe52e82f211d9afd54835
redcreeper14385 <mounderfod@gmail.com>
2021-07-01 18:24:29 +0100

    Resolve #13: Remove NBT Crafting Make gripped tools use the smithing table, remove NBT Crafting as a dependency

--- src/main/java/mounderfod/fabricforfabric/registry/UtilityItems.java
+++ src/main/java/mounderfod/fabricforfabric/registry/UtilityItems.java
@@ -14,9 +14,11 @@ import static mounderfod.fabricforfabric.FabricForFabric.FABRIC_TOOLS;
 public class UtilityItems {
     // Bandage
     public static final Item BANDAGE = new ItemWithTooltip(new Item.Settings().group(FABRIC_TOOLS).maxCount(16).food(new FoodComponent.Builder().alwaysEdible().hunger(0).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20*5), 1.0f).build()), new TranslatableText[]{new TranslatableText("item.fabricforfabric.bandage.tooltip")});
+    public static final Item GRIP = new ItemWithTooltip(new Item.Settings().group(FABRIC_TOOLS).maxCount(1), new TranslatableText[]{new TranslatableText("item.fabricforfabric.grip.tooltip")});
 
     public static void register() {
         // Bandage
         Registry.register(Registry.ITEM, new Identifier("fabricforfabric", "bandage"), BANDAGE);
+        Registry.register(Registry.ITEM, new Identifier("fabricforfabric", "grip"), GRIP);
     }
 }
